5e2875a188ca32e28c3b30aa00312c5df78a5ebc
Aryan Shravan Chouti <aryanshravanchouti123@gmail.com>
2026-08-01 23:50:16 +0530

    fix: remove duplicate initLoader export (#3983)
    Co-authored-by: Esha Jha <149310101+Eshajha19@users.noreply.github.com>

--- modules/loader.js
+++ modules/loader.js
@@ -1,10 +1,11 @@
-
+  
 export function initLoader() {
   setTimeout(() => {
     const loader = document.getElementById("loading-screen");
-
     if (loader) {
       loader.classList.add("hidden");
     }
   }, 1000);
 }
+
+window.initLoader = initLoader;
